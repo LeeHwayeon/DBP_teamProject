527c885d37a4634b90d3537115b009330ee8ecb2
JY <pyeonjy97@gmail.com>
2018-05-10 23:38:57 +0900

    navbar

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// 메인 페이지
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { state: 'beforeLogin' });
+});
+
+// 회원가입
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { state: 'beforeLogin' });
+});
+
+// 로그인
+router.get('/signin', function(req, res, next) {
+  res.render('signin', { state: 'beforeLogin' });
 });
 
 module.exports = router;
