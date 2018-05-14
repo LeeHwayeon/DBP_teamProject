c2f72351f560ee3dfb4ae48e64f8f7688d01b7e4
limhyejin <hejin8307@naver.com>
2018-05-14 13:33:41 +0900

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var oracledb = require('oracledb');
+var dbConfig = require('../oracle/dbconfig');
 
-/* GET home page. */
+// 메인 페이지
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  oracledb.getConnection(dbConfig, (err, connection) => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+    connection.execute("select * from users", (err, result) => {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+      console.log(result.rows);
+      res.render('index', { state: 'beforeLogin', result: result.rows });
+    });
+  });
+});
+
+// 회원가입
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { state: 'beforeLogin' });
+});
+
+// 로그인
+router.get('/signin', function(req, res, next) {
+  res.render('signin', { state: 'beforeLogin' });
+});
+
+//회원가입 가입번호 입력
+router.get('/authentication', function(req, res, next) {
+  res.render('authentication', { state: 'beforeLogin' });
+});
+
+//프로젝트 페이지
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { state: 'beforeLogin' });
 });
 
 module.exports = router;
